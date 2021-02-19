b1e6e38d837e974adada42f2c3194390be34f01f
masanor57 <miyajim2020@gmail.com>
2021-02-19 09:57:17 +0000

    Finish アプリケーションを完成させよう2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '65a154d7da46e6a5d8a5d1fe97c9bd38670a95eb125d77d920470a0611c702904713a55562e875bb2719cf935efc91a5d9d570e58f64dcc6ca879de1a64cce2e'
